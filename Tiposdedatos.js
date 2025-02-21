748aa873a6e017676545fef8ee90119c398e9061
Ceciliana98 <anacecilia_gm@hotmail.com>
2025-02-20 21:00:06 -0600

    tipos de datos en javascript

--- Tiposdedatos.js
+++ Tiposdedatos.js
@@ -1,1 +1,17 @@
- 
+/* 
+Tipos de datos
+*/
+
+console.log(typeof(25));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof("true"));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(' '));
+console.log(typeof(""));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+console.log(typeof(false));
